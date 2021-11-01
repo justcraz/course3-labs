4eb18ca91c5bfc05b35a108b63a936639f2aa333
Alexandr Grigoraschuk <64975380+justcraz@users.noreply.github.com>
2021-11-01 14:42:59 +0200

    Update lab1ex2.c

--- lab1ex2.c
+++ lab1ex2.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main() { int a[20][10], j, k, jmin, jmax,
-kmin, kmax;
-double min, max, b[4][4], c;
+
+int main()
+{
+const int m = 5, n = 7;
+int Matrix[m][n];
 srand(time(NULL));
-for (j = 0; j < 20; j++) {
-for (k = 0; k < 10; k++) { a[j][k] = rand() % 201
-- 100;
-printf("%5d", a[j][k]);
+/* генерація та показ початкової матриці */
+for(int x = 0; x < m; x++)
+{
+for(int y = 0; y < n; y++)
+{
+Matrix[x][y] = rand() % 201 - 100;
+printf("%d\t", Matrix[x][y]);
 }
-printf("\n");
+putchar('\n');
 }
-scanf("%lf", &b[0][0]);
-max = min = b[0][0];
-for (j = 0; j < 4; j++) for (k = 0; k < 4; k++)
-{ scanf("%lf", &b[j][k]);
-if (b[j][k] < min) {
-min = b[j][k]; jmin = j; kmin = k;
+/* шукаємо індекси мінімального та максимальний елементи */
+int i_max_x = 0, i_max_y = 0, i_min_x = 0, i_min_y = 0;
+for(int x = 0; x < m; x++)
+for(int y = 0; y < n; y++)
+{
+if(Matrix[x][y] > Matrix[i_max_x][i_max_y])
+{
+i_max_x = x;
+i_max_y = y;
 }
-if (b[j][k] >
-max) { max = b[j][k];
-jmax = j;
-kmax = k;
+if(Matrix[x][y] < Matrix[i_min_x][i_min_y])
+{
+i_min_x = x;
+i_min_y = y;
 }
 }
-c = b[jmin][kmin];
-b[jmin][kmin] = b[jmax][kmax];
-b[jmax][kmax] = c;
-for (j = 0; j < 4; j++) {
-for (k = 0; k < 4; k++)
-printf("%10.6f", b[j][k]);
-printf("\n");
+/* змінюємо значення мінімуму та максимуму */
+int tmp = Matrix[i_max_x][i_max_y];
+Matrix[i_max_x][i_max_y] = Matrix[i_min_x][i_min_y];
+Matrix[i_min_x][i_min_y] = tmp;
+/* виводь на друк змінену матрицю */
+putchar('\n');
+for(int x = 0; x < m; x++)
+{
+for(int y = 0; y < n; y++)
+printf("%d\t", Matrix[x][y]);
+putchar('\n');
 }
+return 0;
 }
